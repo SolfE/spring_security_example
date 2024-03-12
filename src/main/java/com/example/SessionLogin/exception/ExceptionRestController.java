f31dc14681231d42e4e6384a2f338f07ca506a15
SolfE <devsolfe@gmail.com>
2024-03-12 23:14:33 +0900

    feat: add excpetion process

---
+++ src/main/java/com/example/SessionLogin/exception/ExceptionRestController.java
@@ -0,0 +1,44 @@
+package com.example.SessionLogin.exception;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/exception-example")
+@RequiredArgsConstructor
+public class ExceptionRestController {
+
+    private final ExceptionService exceptionService;
+
+    @GetMapping("/throw-my-exception/1")
+    public void throwMyException1() {
+        exceptionService.login();
+    }
+
+    @GetMapping("/throw-my-exception/2")
+    public void throwMyException2() {
+        exceptionService.writeComment();
+    }
+
+    @GetMapping("/throw-my-exception/3")
+    public void throwMyException3() {
+        exceptionService.join();
+    }
+
+    @GetMapping("/throw-my-exception/4")
+    public void throwMyException4() {
+        exceptionService.editComment();
+    }
+
+    // ExceptionRestController.class 내부에서 MyException.class 에러가 발생하면 해당 핸들러 호출
+    @ExceptionHandler(MyException.class)
+    public ResponseEntity<?> myExceptionHandler(MyException e) {
+        e.printStackTrace();
+        return ResponseEntity.status(e.getErrorCode().getStatus())
+                .body(new ExceptionDto(e.getErrorCode()));
+    }
+}
